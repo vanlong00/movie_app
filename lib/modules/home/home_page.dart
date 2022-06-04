9c9f3fd01efdf789b622b63eaa67b0b7f38730db
vanlong00 <fly.ride777@gmail.com>
2022-06-05 06:51:03 +0700

    update categorybar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/modules/home/components/category_bar.dart';
 import 'package:movie_app/modules/home/components/header.dart';
 import 'package:movie_app/modules/home/components/search_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -16,8 +22,11 @@ class HomePage extends StatelessWidget {
           HomeHeader(size: size),
           //search bar
           SearchBar(size: size),
+          //category bar
+          CategoryBar(size: size)
         ],
       )),
     );
   }
 }
+
